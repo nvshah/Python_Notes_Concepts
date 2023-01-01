548324807170851a403751c57aa88d3076a8a40b
Nipun Shah <nipunshah6776@gmail.com>
2023-01-02 00:42:34 +0530

    new notes

--- concept code/tricky/tuple_unpacking.py
+++ concept code/tricky/tuple_unpacking.py
@@ -4,3 +4,12 @@ from re import S
 print(True, True, True == (True, True, True))  # True, True, False
 
 print((True, True, True) == (True, True, True)) # True
+
+### Pattern UnPacking
+
+a, (b, c) = (1, (2, 3))
+# a = 1, b=2, c=3
+
+a, (b, *c) = (1, (10, 20, 30))
+# a=1, b=10, c=[20, 30]
+
